31cdf6548c6c41cfbd4139fe7f476351e8afe141
James McRay Johnson III <james@uptownjimmy.com>
2018-12-28 20:10:39 -0500

    Shopping List: add update shopping list modal

--- src/app/shopping/update/update.modal.ts
+++ src/app/shopping/update/update.modal.ts
@@ -1,52 +1,50 @@
-import { Component, OnInit, Input, Inject } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import {Component, OnInit, Input, Inject} from '@angular/core';
+import {ModalController} from '@ionic/angular';
 
-import { Item } from '../../item/item.model';
+import {Item} from '../../item/item.model';
 
 @Component({
-    selector: 'us-add-items',
-    templateUrl: './update.modal.html',
-    styleUrls: ['./update.modal.css']
+	selector: 'us-add-items',
+	templateUrl: './update.modal.html',
+	styleUrls: ['./update.modal.css']
 })
 export class ShoppingListUpdateModal implements OnInit {
-    @Input() items: Item[];
+	@Input() items: Item[];
 
-    private loading = false;
-    private initialItems: Item[];
-    private updateItems: Item[] = [];
+	private loading = false;
+	private initialItems: Item[];
+	private updateItems: Item[] = [];
 
-    constructor(
-        @Inject('itemService') private itemService,
+	constructor(
+		@Inject('itemService') private itemService,
 		public modalController: ModalController
 	) {
-        this.itemService.getItemsSnapshot().subscribe(
-            (response: Item[]) => {
-                this.initialItems = response;
-            }
-        );
-    }
+		this.itemService.getItemsSnapshot().subscribe(
+			(response: Item[]) => {
+				this.initialItems = response;
+			}
+		);
+	}
 
-    ngOnInit() {}
+	ngOnInit() {}
 
-    private onItemListClick(item: Item) {
-        const changedItem = this.initialItems.find(
-            initialItem => {
-                return initialItem.id === item.id && initialItem.active !== item.active;
-            }
-        );
+	private onItemListClick(item: Item) {
+		const changedItem = this.initialItems.find(initialItem =>
+			initialItem.id === item.id && initialItem.active !== item.active
+		);
 
-        if (changedItem) {
-            this.updateItems.push(item);
-        }
-    }
+		if (changedItem) {
+			this.updateItems.push(item);
+		}
+	}
 
-    private addToShoppingList() {
-        this.loading = true;
-        this.updateItems.forEach(item => {
-            this.itemService.updateExistingItem(item);
-        });
+	private addToShoppingList() {
+		this.loading = true;
+		this.updateItems.forEach(item => {
+			this.itemService.updateExistingItem(item);
+		});
 
 		this.modalController.dismiss();
 		this.loading = false;
-    }
+	}
 }
