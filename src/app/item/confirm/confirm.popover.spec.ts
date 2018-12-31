9c48f3e7e98cf3f02664ab31b34bc678096152d7
James McRay Johnson III <james@uptownjimmy.com>
2018-12-30 19:39:33 -0500

    create confirm delete item popover,  reimplement delete item

---
+++ src/app/item/confirm/confirm.popover.spec.ts
@@ -0,0 +1,25 @@
+// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+//
+// import { OptionsModal } from './shopping-item-options.component';
+//
+// describe('OptionsModal', () => {
+//   let component: OptionsModal;
+//   let fixture: ComponentFixture<OptionsModal>;
+//
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ OptionsModal ]
+//     })
+//     .compileComponents();
+//   }));
+//
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(OptionsModal);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+//
+//   it('should be created', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
